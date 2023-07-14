9c1482914f38bd2d985da8cf50b8002457d53619
shsiskavlesu <kolasafronenko55@gmail.com>
2023-07-14 18:41:59 +0300

    init

--- my_app.py
+++ my_app.py
@@ -1,1 +1,85 @@
 # напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+ 
+ 
+from instr import *
+from second_win import *
+ 
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.connects()
+        self.set_appear()
+        self.show()
+ 
+ 
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next, self)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+ 
+ 
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout_line)
+ 
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+ 
+ 
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+ 
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+ 
+ 
+app = QApplication([])
+mw = MainWin()
+app.exec_()
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+ 
+ 
+txt_hello = 'Добро пожаловать в программу по определению состояния здоровья!'
+txt_next = 'Начать'
+txt_instruction = ('Данное приложение позволит вам с помощью теста Руфье провести первичную диагностику вашего здоровья.\n'
+                   'Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособности сердца при физической нагрузке.\n'
+                   'У испытуемого, находящегося в положении лежа на спине в течение 5 мин, определяют частоту пульса за 15 секунд;\n'
+                   'затем в течение 45 секунд испытуемый выполняет 30 приседаний.\n'
+                   'После окончания нагрузки испытуемый ложится, и у него вновь подсчитывается число пульсаций за первые 15 секунд,\n'
+                   'а потом — за последние 15 секунд первой минуты периода восстановления.\n'
+                   'Важно! Если в процессе проведения испытания вы почувствуете себя плохо (появится головокружение, шум в\n'
+                   'ушах, сильная одышка и др.), то тест необходимо прервать и обратиться к врачу.' )
+txt_title = 'Здоровье'
+txt_name = 'Введите Ф.И.О.:'
+txt_hintname = "Ф.И.О."
+txt_hintage = "0"
+txt_test1 = 'Лягте на спину и замерьте пульс за 15 секунд. Нажмите кнопку "Начать первый тест", чтобы запустить таймер.\nРезультат запишите в соответствующее поле.'
+txt_test2 = 'Выполните 30 приседаний за 45 секунд. Для этого нажмите кнопку "Начать делать приседания",\nчтобы запустить счетчик приседаний.'
+txt_test3 = 'Лягте на спину и замерьте пульс сначала за первые 15 секунд минуты, затем за последние 15 секунд.\nНажмите кнопку "Начать финальный тест", чтобы запустить таймер.\nЗелёным обозначены секунды, в течение которых необходимо \nпроводить измерения, черным - секунды без замера пульсаций. Результаты запишите в соответствующие поля.'
+txt_sendresults = 'Отправить результаты'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'Начать первый тест'
+txt_starttest2 = 'Начать делать приседания'
+txt_starttest3 = 'Начать финальный тест'
+txt_timer = ''
+ 
+ 
+txt_age = 'Полных лет:'
+txt_finalwin = 'Результаты'
+txt_index = 'Индекс Руфье: '
+txt_workheart = 'Работоспособность сердца: '
